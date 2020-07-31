3cab6c895e521688dc868c6d21d496b8334a1502
JRG <47917431+JR-G@users.noreply.github.com>
2020-07-31 13:52:12 +0100

    Created tests (new kata)

--- lib/consec_letters.rb
+++
@@ -1,7 +0,0 @@
-def solve(st)
-  if st == "abc"
-    return true
-  elsif st == "abd"
-    return false
-  end
-end
--- spec/consec_letters_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'consec_letters'
-describe 'solve' do
-  it 'returns true when passed "abc"' do
-    expect(solve("abc")).to eq true
-  end
-  it 'returns true when passed "abd"' do
-    expect(solve("abd")).to eq false
-  end
-end
